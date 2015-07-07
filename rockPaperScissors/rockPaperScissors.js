de99fdf9a7bba54d92cd91fa88c70216557e512b
Eric Fischer <ericf712@gmail.com>
2015-07-07 09:53:53 -0700

    Finish recursive rock-paper-scissors

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,27 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
-};
+var rockPaperScissors = function (n) {
+  var rounds = n;
+  var combinations = [];
+  var choices = ['rock', 'paper', 'scissors'];
+
+  var recurse = function(roundsLeft, handPlayed) {
+    if (roundsLeft === 0) {
+      combinations.push(handPlayed);
+      return;
+    } else {
+      for (var i=0; i<choices.length; i++) {
+        var current = choices[i];
+        recurse(roundsLeft - 1, handPlayed.concat(current));
+
+      }
+    }
+  };
+  recurse(rounds, []);
+
+  return combinations;
+}
+
+console.log(rockPaperScissors(3));
+
