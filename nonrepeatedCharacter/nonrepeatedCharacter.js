2344185a6ff5a038dd37b3fbb91d2c9ffbe2ddb7
Eric Fischer <ericf712@gmail.com>
2015-07-12 22:37:29 -0700

    Optimizes non-repeated character

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,33 +6,23 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-// var firstNonRepeatedCharacter = function(string) {
-//   var array = string.split("");
-//   for (var i=0; i<array.length; i++) {
-//     if (array[i] !== array[i+1]) {
-//       return array[i+1];
-//     }
-//   }
-// };
-
 
 var firstNonRepeatedCharacter = function(string) {
-  var chars = {};
+  var obj = {};
   for (var i = 0; i < string.length; i++) {
-    character = string[i];
-    if (!chars[character]) {chars[character] = 1;}
-    else {chars[character]++;}
+    letter = string[i];
+    if (!obj[letter]) {obj[letter] = 1;}
+    else {obj[letter]++;}
   }
 
   for (var i=0; i < string.length; i++) {
-    character = string[i];
-    if (chars[character] === 1) {return character;}
+    letter = string[i];
+    if (obj[letter] === 1) {return letter;}
   }
   return null;
 }
 
-// Both for loops are linear for a time complexity of 2n --> n. (Nested for loops (worse) create
-// a time complexity of n^2.)
+// Both for loops are linear for a time complexity of 2n -> n. Nested for loops (worse) create a time complexity of n^2.
 
-var result = firstNonRepeatedCharacter('CBAB');
-console.log(result);
+// var result = firstNonRepeatedCharacter('CBAB');
+// console.log(result);
