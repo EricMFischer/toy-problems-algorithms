3a9da725230d1ce8fce8f030613a6b8d0c03833b
Eric Fischer <ericf712@gmail.com>
2015-07-12 22:38:09 -0700

    Optimizes nthFibonacci

--- nthFibonacci/nthFibonacci.js
+++ nthFibonacci/nthFibonacci.js
@@ -4,7 +4,7 @@
  *
  * For example, the first five Fibonacci numbers are:
  *
- *   0 1 1 2 3
+ *  0 1 1 2 3
  *
  * If n were 4, your function should return 3; for 5, it should return 5.
  *
@@ -17,52 +17,33 @@
  * nthFibonacci(3); // => 2
  * nthFibonacci(4); // => 3
  * etc...
- *
  */
 
-var nthFibonacci = function (n) {
-  // create an array that does what Fibonacci does
-  var fibonacci = [0];
-  var recurse = function(n) {
-    if (n === 0) {
-      return;
-    } else {
-      for (var i = 1; i <= n; i++) {
-        fibonacci[i] = (fibonacci[i-1] + (fibonacci[i-2]) || 1(fibonacci[i-1] + (fibonacci[i-2] || i) )) + fibonacci[i-1];
-                        // hmm... how do I derive fibonacci[i] before it exists... ? (fibonacci[i-1] + (fibonacci[i-2] || i) )
-      }
-      recurse(n-1);
-    }
-  }
-  recurse(n);
+// var nthFibonacci = function (n) {
+//   // create an array that does what Fibonacci does
+//   var fibonacci = [0, 1];
+//   var recurse = function(n) {
+//     if (n === 0) {
+//       return;
+//     } else {
+//       for (var i = 2; i <= n; i++) {
+//         fibonacci[i] = (fibonacci[i-1] + fibonacci[i-2]);
+//       }
+//       recurse(n-1);
+//     }
+//   }
+//   recurse(n);
 
-  return fibonacci//[n];
-};
-
-console.log(nthFibonacci(4));
+//   return fibonacci//[n];
+// };
+// console.log(nthFibonacci(4));
 
 // Solution
-
-var nFib = function(n) {
-  return n < 2 ? n : nFib(n-1) + nFib(n-2);
+var fibRecurse = function(n) {
+  return n < 2 ? n : fibRecurse(n-1) + fibRecurse(n-2);
 }
 
-// As we increment up, we save each value in the array. so we'd just be accessing the last two places in our array
-// without having to calculate them again
-
-// linear time
-
-var nFib = function(n) {
-  var mem = [];
-  for (var i=0; i<n; i++) {
-    mem[i] = i < 2 ? i : mem[i-1] + mem[i-2];
-  }
-  return mem[n];
-}
-
-// you can optimize it more (for constant time) by storing the nFib.mem array outside of the function. that way
-// you're just looking up values in a global score if you were to call it again with the same argument
-
+// As we increment up, we save each value in the array. So we'd just be accessing the last two places in our array without having to calculate them again. This is linear time.
 var fibMem = function(n) {
   var mem = [];
   for (var i=0; i<n; i++) {
@@ -71,12 +52,18 @@ var fibMem = function(n) {
   return mem[n];
 }
 
-fibMem/mem = [];
+// You can optimize it more (for constant time) by storing the nFib.mem array outside of the function. That way you're just looking up values in a global scope if you were to call it again with the same argument
+var fibMem = function(n) {
+  if (fibMem.mem[n]) {return fibMem.mem[n];}
+  for (var i=0; i<=n; i++) {
+    fibMem.mem[i] = i < 2 ? i : fibMem.mem[i-2] + fibMem.mem[i-1];
+  }
+  return fibMem.mem[n];
+}
+fibMem.mem = [];
 
-// Even more simple. space complexity is constant because we're disregarding the previous n's with the exception
-// of the last two
-
-var nFib = function(n) {
+// Even more simple. Space complexity is constant because we're disregarding the previous n's with the exception of the last two
+var fibSpace = function(n) {
   var mem = [0,1];
   for (; n > 1; n--) {
     mem.push(mem.shift() + mem[0]);
