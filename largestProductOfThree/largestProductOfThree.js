7c34d4272d6c90a4fa34021b3df4141af3907642
Eric Fischer <ericf712@gmail.com>
2015-07-16 09:41:24 -0700

    Completes largestProductOfThree

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,7 +7,18 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-
+// Sort compare function
+// if return answer is:
+// positive --> b goes first
+// negative --> a goes first
 var largestProductOfThree = function(array) {
-  // TODO: everything
+  array.sort(function(a,b) {return b-a;});
+  var result = 1;
+  for (var i=0; i<3; i++) {
+    result *= array[i];
+  }
+  return result;
 };
+
+
+console.log(largestProductOfThree([5, 10, 4, 6, 2, 3, 9, 4]));
