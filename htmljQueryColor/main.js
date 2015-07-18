4867c180817ede7ca174c81621fa47a7b66ce3ab
Eric Fischer <ericf712@gmail.com>
2015-07-18 10:35:23 -0700

    Attempt at htmljQueryColor

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -6,11 +6,22 @@ $(function(){
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
+  var spanWrap = function(text) {
+    return '<span>' + text.split(" ").join('</span><span> ') + '</span>';
+  }
+  var paragraphText = $("p")[0].innerHTML;
 
-
+  var wrapped = spanWrap(paragraphText);
+  $("p")[0].innerHTML = wrapped;
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
+  var randomColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
 
-  // TODO: your code here!
+  $(document).ready(function() {
+    var spans = $('span');
+    for (var i=0; i<spans.length; i++) {
+      $(spans[i]).css('color', randomColor());
+    }
+  });
 
 });
