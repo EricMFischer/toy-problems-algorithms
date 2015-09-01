94c468276022aa943f238bac20667bb3a9279afb
Eric Fischer <ericf712@gmail.com>
2015-09-01 10:12:08 -0700

    Completes characterFrequency

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -37,5 +37,22 @@
 
 
 var characterFrequency = function(string) {
-  return result;
+  var tracker = {};
+  for (var i=0; i<string.length; i++) {
+    var letter = string[i];
+    if (!tracker[letter]) {
+      tracker[letter] = 1;
+    } else {
+      tracker[letter]++;
+    }
+  }
+
+  var array = [];
+  for (var key in tracker) {
+    array.push([key, tracker[key]]);
+  }
+  array.sort(function(a,b) {return b[1] - a[1]});
+  return array;
 };
+
+console.log(characterFrequency('mississippi'));
