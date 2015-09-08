2a49939a9178c17ae8585ea8d9f56a412d9b9d50
Eric Fischer <ericf712@gmail.com>
2015-09-08 10:06:15 -0700

    Adds functionBind

--- functionBind/functionBind.js
+++ functionBind/functionBind.js
@@ -23,10 +23,30 @@
  *
 */
 
-var bind = function(
-) {
-  // TODO: Your code here
+var bind = function(func, context) {
+  var outerArgs = Array.prototype.slice.call(arguments, 2);
+  return function() {
+    var innerArgs = Array.prototype.slice.call(arguments);
+    return func.apply(context, outerArgs.concat(innerArgs));
+  };
 };
+
+var alice = {
+  name: 'alice',
+  shout: function(){
+    console.log(this.name);
+  }
+}
+
+var boundShout = bind(alice.shout, alice);
+boundShout(); // alerts 'alice'
+boundShout = bind(alice.shout, {name: 'bob'});
+console.log(boundShout()); // alerts 'bob'
+
+var func = function(a, b){ return a + b };
+var boundFunc = bind(func, null, 'foo');
+var result = boundFunc('bar');
+console.log(result === 'foobar'); // true
 
 /*
  * Function.prototype.bind:
@@ -53,7 +73,26 @@ var bind = function(
  *
 */
 
-Function.prototype.bind = function(
-) {
+Function.prototype.bind = function(context) {
   // TODO: Your code here
+  var outerArgs = Array.prototype.slice.call(arguments, 1);
+  var func = this;
+  return function () {
+    var innerArgs = Array.prototype.slice.call(arguments);
+    console.log(innerArgs);
+    return func.apply(context, outerArgs.concat(innerArgs));
+  };
 };
+
+
+// ---------------------------------------- // 
+
+var boundShout = alice.shout.bind(alice);
+boundShout(); // alerts 'alice'
+boundShout = alice.shout.bind({name: 'bob'});
+boundShout(); // alerts 'bob'
+
+var func = function(a, b){ return a + b };
+var boundFunc = func.bind(null, 'foo');
+var result = boundFunc('bar');
+console.log(result === 'foobar'); // true
