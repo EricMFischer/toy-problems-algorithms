e66b429420aa744b2f795f4ba23b130730d8d624
Eric Fischer <ericf712@gmail.com>
2015-12-24 03:00:41 -0800

    Complete

--- binaryHeap/binaryHeap.js
+++ binaryHeap/binaryHeap.js
@@ -1,15 +1,10 @@
 /**
- * A heap is a special kind of tree in which a parent node is ordered only in
- * respect to its immediate children. Unlike the binary search tree you may have
- * implemented, where the entire tree is ordered, in a heap the only order
- * guaranteed is between a node and its parent.
+ * A heap is a special tree in which a parent node is ordered only in
+ * respect to its immediate children, unlike binary trees where it is all ordered.
  *
  * In a binary heap each node should have only zero, one, or two children. Each node
- * must have 2 children before the next oldest node can have any children. Therefore
- * the 0th node will be the parent of the 1st and 2nd nodes, the 1st node will be the
- * parent of the 3rd and 4th nodes, and the 2nd node will be the parent of the 5th and
- * 6th nodes. In a specific kind of binary heap, the binary min heap, every node is
- * less than its immediate children:
+ * must have 2 children before the next oldest node can have any children, i.e. it is a complete tree.
+ * In a specific kind of binary heap, the binary min heap, every node is less than its immediate children:
  * 
  *          0
  *     1         2
@@ -17,120 +12,119 @@
  *  7
  *
  * There is only one place at any given time in a binary heap where a node can be
- * added or removed. In the example above, the next node will be inserted as the second
- * child of 3. If we were to remove a node instead, we would remove the 7. This mimics
- * the behavior of a stack and allows us to manage the heap in a very memory efficient way,
- * using a list or array. For example, the heap pictured above can be described as:
+ * added or removed: the end. Thus, we can manage the heap in a very memory efficient way,
+ * using a list or array:
  *
  * [0, 1, 2, 3, 4, 5, 6, 7]
  *
- * where we always add to or remove from the end of the array.
- *
- * A well known fact, utilized with binary heaps stored in arrays, is that
- * we can calculate the index of a node's parent or children using math:
- *
+ * For each node in a binary heap:
  * parentIndex = Math.floor( (index - 1) / 2 )
  * childrenIndices = [index * 2 + 1, index * 2 + 2]
  *
- * When adding a new node to a binary min heap, it could be that we violate the property of the
- * heap whereby every node is of lower value than its children. Thus whenever we insert into
- * a binary min heap, we must compare the inserted node to its parent, and swap their positions
+ * For Binary MIN Heaps, insert and remove from the END.
+ * INSERT: We must compare the inserted node to its parent, and swap their positions
  * if it is less than its parent. After a swap it must compare itself to its new parent, continuing
- * until it is no longer less than its parent.
+ * until it is more than its parent.
  *
- * Something similar happens when we want to remove the root node. Because we can only remove from the
- * end of the array we swap the position of the last node and the root node and then remove the now-last
- * node from the heap. The new root node now must be compared to its children and if it is not less than
- * both of them, be swapped with whichever of the two of them is the smallest. It is then compared with its
- * new children and this swapping continues until it is less than both its children.
+ * REMOVE: We swap the position of the last node and the root node and now remove the last
+ * node from the heap. The new root node now must be compared to its children and, if it is not less than
+ * both of them, it must be swapped with the smaller of the two. This swapping continues until it is less than both its children.
  *
- * You can see a great visualization of a binary min heap in action here, play around with it until you can
- * easily guess how the heap will behave with both insertion and removal:
+ * You can see a great visualization of a binary min heap in action here:
  * https://www.cs.usfca.edu/~galles/visualization/Heap.html
  */
 
-
-// Below is a binary heap whose nodes are integers. Its storage is an array and
-// its `getRoot` method is already written. `BinaryHeap`'s `this._compare` method is hard-coded to return
-// whether the fist element passed into it is less than the second. Use it when comparing nodes.
-//
+// Below is a binary heap whose nodes are integers. 
 // Implement the `insert` and `removeRoot` methods, each operating in logarithmic time relative
-// to the size of the heap, and each restoring the heap's property of parent to child sorting. Use
-// the equations above to navigate parent / child relationships in the storage array, and write any
-// helper functions needed to assist you.
+// to the size of the heap, and each restoring the heap's property of parent to child sorting.
 //
-// Extra credit: `BinaryHeap`'s `this._compare` is hard-coded to assist in making a min heap, modify `BinaryHeap`
+// Extra credit: `BinaryHeap`'s `this._compare` is hard-coded to assist in making a min heap. Modify `BinaryHeap`
 // to accept an optional argument which is a function used as the sorting mechanism for the heap.
 // That way you can use your `BinaryHeap` class to construct a max heap or min heap or whatever.
 //
 // Extra extra credit: Implement `heapSort`. `heapSort` takes an array, constructs it into a `BinaryHeap`
 // and then iteratively returns the root of the `BinaryHeap` until its empty, thus returning a sorted array.
 
-// Heaps are complete trees--a node must have its children filled before the next node can have children
-// Constant time?
-// Dijkstra's Algorithm
 function BinaryHeap () {
   this._heap = [];
-  // this compare function will result in a minHeap, use it to make comparisons between nodes in your solution
-  this._compare = function (i, j) { return i < j }; // returns true if 
+  this._compare = function (i, j) { return i < j };
 }
 
-// This function works just fine and shouldn't be modified
+// This function shouldn't be modified
 BinaryHeap.prototype.getRoot = function () {
   return this._heap[0];
 }
 
 BinaryHeap.prototype.swapNodesAt = function(index, parentIndex) {
-
+  var parent = this._heap[parentIndex];
+  this._heap[parentIndex] = this._heap[index];
+  this._heap[index] = parent;
 }
 
- // * parentIndex = Math.floor( (index - 1) / 2 )
- // * childrenIndices = [index * 2 + 1, index * 2 + 2]
-// whenever we insert into
-//  * a binary min heap, we must compare the inserted node to its parent, and swap their positions
-//  * if it is less than its parent. After a swap it must compare itself to its new parent, continuing
-//  * until it is no longer less than its parent.
-BinaryHeap.prototype.insert = function (value) {
+// INSERT: When we insert into a binary min heap, we must compare the inserted node to its parent, and swap their positions
+// if it is less than its parent. After a swap it must compare itself to its new parent, continuing
+// until it is no longer less than its parent.
+
+BinaryHeap.prototype.insert = function(node) {
   this._heap.push(node);
-  var currIndex = this._heap.length - 1;
-  var parentIndex = Math.floor((currIndex - 1) / 2);
-  // var parentValue = this._heap[Math.floor((currIndex - 1) / 2)];
-  while (index > 0 && (this._compare(this._heap[index], this._heap[parentIndex]))) {
-    swapNodesAt(index, parentIndex);
-    currIndex = parentIndex;
-    parentIndex = Math.floor((index-1) / 2);
+  var insertIndex = this._heap.length - 1;
+  var parent = this._heap[Math.floor((insertIndex-1) / 2)];
+  var children = [this._heap[insertIndex*2+1], this._heap[insertIndex*2+2]];
+
+  while (insertIndex > 0 && this._compare(node, parent)) {
+    var oldParentIndex = Math.floor((insertIndex-1) / 2);
+    this.swapNodesAt(insertIndex, oldParentIndex);
+    insertIndex = oldParentIndex;
+    parent = this._heap[Math.floor((insertIndex-1) / 2)]; // redefine parent to move through the tree and bring a terminating cond. to while loop
   }
+  return this._heap;
 }
 
-BinaryHeap.prototype.removeRoot = function () {
-  // swap the accessible node (last one in array) with root
-  swapNodesAt(this._heap.length-1, 0, this);
-  // remove last node and store it to be returned later
-  var originalRoot = this._heap.pop();
-  var tempRootIndex = 0;
-  // locate children nodes
-  var childIndices = [parentIndex * 2 + 1, parentIndex * 2 + 2].filter(function)..
-  var lesserChildIndex = getLesserChildIndex(tempRootIndex, this);
-  // while there are chilren nodes and the lesser of them is less than the new root
-  while (lesserChildIndex && this._compare(this._heap[lesserChildIndex], this._heap[tempRootIndex])) {
-    swapNodesAt(lesserChildIndex, tempRootIndex, this);
+// REMOVE: We swap the position of the last node and the root node and now remove the last
+// node from the heap. The new root node now must be compared to its children and, if it is not less than
+// both of them, it must be swapped with the smaller of the two. This swapping continues until it is less than both its children.
+
+BinaryHeap.prototype.removeRoot = function() {
+  var root = this.getRoot();
+  var lastNode = this._heap[this._heap.length - 1];
+  this.swapNodesAt(0, this._heap.length - 1);
+  this._heap.pop();
+
+  var newRoot = this.getRoot();
+  var index = 0;
+  var child1 = this._heap[1];
+  var child2 = this._heap[2];
+  while (child1 !== undefined && child2 !== undefined && (newRoot > child1 || newRoot > child2)) {
+    if (child1 <= child2) {
+      this.swapNodesAt(index * 2 + 1, index);
+      newRoot = this._heap[index * 2 + 1];
+      index = index * 2 + 1;
+      child1 = this._heap[index * 2 + 1];
+      child2 = this._heap[index * 2 + 2];
+    } else {
+      this.swapNodesAt(index * 2 + 2, index);
+      newRoot = this._heap[index * 2 + 2];
+      index = index * 2 + 2;
+      child1 = this._heap[index * 2 + 1];
+      child2 = this._heap[index * 2 + 2];
+    }
   }
-  // then worry about your sorting
+  return this._heap;
 }
 
+var binaryHeap = new BinaryHeap();
+binaryHeap.insert(4);
+binaryHeap.insert(5);
+binaryHeap.insert(9);
+binaryHeap.insert(8);
+binaryHeap.insert(1);
+binaryHeap.insert(0);
+binaryHeap.removeRoot();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var compare = binaryHeap._compare;
+var heap = binaryHeap._heap;
+// should be all true:
+console.log(compare(heap[0], heap[1])); // (1,4)
+console.log(compare(heap[0], heap[2])); // (1,9)
+console.log(compare(heap[1], heap[3])); // (4,8)
+console.log(compare(heap[1], heap[4])); // (4,5)
