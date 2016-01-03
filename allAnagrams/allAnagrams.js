dfd85e7625976d8fa19a9101aed227b6f589716a
Eric Fischer <ericf712@gmail.com>
2016-01-02 19:28:03 -0800

    Complete

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,21 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
-  // Your code here.
-};
+var allAnagrams = function(str) {
+  var uniqOutput = {};
+  var anagram = function(ana, restOfStr) {
+    if (restOfStr === '') {
+      uniqOutput[ana] = true;
+      return;
+    }
+    for (var i=0; i<restOfStr.length; i++) {
+      // console.log(ana + restOfStr[i], '|', restOfStr.slice(0,i) + restOfStr.slice(i+1));
+      anagram(ana + restOfStr[i], restOfStr.slice(0,i) + restOfStr.slice(i+1));
+    }
+  }
+  anagram('', str);
+  return Object.keys(uniqOutput);
+}
+
+var anagrams = allAnagrams('abc');
+console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
